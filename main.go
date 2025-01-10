14d288733c887be54296320640020d81083580fa
zeeshan jatt <zeeshannjatt@gmail.com>
2025-01-10 07:56:17 -0800

    two

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+	fmt.Println("2 time")
 
+}
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
